1b0c01438a023ba1daf5a1d27ccb4dac9f34fd74
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-01-09 13:52:10 -0500

    refactor vite.config.js to support environment-based configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    host: true,
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+
+  return {
+    base: isDev ? "/" : "/static/",
+    plugins: [react()],
+    server: {
+      port: 5173,
+      host: true,
     },
-  },
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
+    },
+  };
 });
