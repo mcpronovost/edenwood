7725a6b5dfbc20e2009bf7daa1bce354cb63e950
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-01-09 13:52:35 -0500

    init prod app

--- backend/edenwood/urls.py
+++ backend/edenwood/urls.py
@@ -1,16 +1,20 @@
 """
-URL configuration for edenwood project.
+URL configuration for Edenwood project.
 """
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("i18n/", include("django.conf.urls.i18n")),
+    path("", RedirectView.as_view(url="/fr/", permanent=False)),
 ] + i18n_patterns(
     path("admin/", admin.site.urls),
+    re_path(r"^(?:.*)?$", TemplateView.as_view(template_name="index.html"))
 )
 
 urlpatterns += static(
