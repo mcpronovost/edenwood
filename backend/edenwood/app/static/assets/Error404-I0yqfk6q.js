18d68bb31a92f4258a1f4bc9fe4b2316d0f2c26e
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-01-09 21:35:39 -0500

    update app

---
+++ backend/edenwood/app/static/assets/Error404-I0yqfk6q.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-KKd3uniT.js";function t(){return r.jsx("div",{children:r.jsx("h1",{children:"Page Not Found"})})}export{t as default};
---
+++ backend/edenwood/app/static/assets/Home-B4kN-N82.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-KKd3uniT.js";function t(){return e.jsx("div",{children:e.jsx("h1",{children:"Welcome to the Home Page"})})}export{t as default};
