0ba914de3578e21df2195e35fcca9b043e821e0c
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-01-10 12:52:35 -0500

    add backend docker entrypoint

---
+++ backend/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/binsh
+set -e
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Starting Gunicorn..."
+exec gunicorn \
+  --bind 0.0.0.0:8000 \
+  --workers 3 \
+  edenwood.wsgi:application
