b494d5ddf4930be2a69ce9e557c044c14ab9993f
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-01-10 22:52:24 -0500

    update app

---
+++ backend/edenwood/app/static/assets/About-DN4IJFFt.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-BMNmMB4z.js";function o(){return t.jsx("div",{children:t.jsx("h1",{children:"Welcome to the About Page"})})}export{o as default};
---
+++ backend/edenwood/app/static/assets/Error404-BCnRCcRZ.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BMNmMB4z.js";function t(){return r.jsx("div",{children:r.jsx("h1",{children:"Page Not Found"})})}export{t as default};
