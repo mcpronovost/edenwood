e6080c278f3c48aee53adf6dc1aa7e92d81de0ef
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-01-13 21:08:30 -0500

    update Card common component

--- frontend/src/components/common/Card.jsx
+++ frontend/src/components/common/Card.jsx
@@ -1,6 +1,28 @@
-export default function EdwCard({ children, nop = false, fh = false, className, style }) {
+export default function EdwCard({
+  children,
+  nop = false,
+  fh = false,
+  alignTop = false,
+  clickable = false,
+  className = "",
+  style,
+  ...props
+}) {
   return (
-    <div className={`edw-card ${nop ? "edw-card-nop" : ""} ${fh ? "edw-card-fh" : ""} ${className ? className : ""}`} style={style}>
+    <div
+      className={[
+        "edw-card",
+        nop ? "edw-card-nop" : "",
+        fh ? "edw-card-fh" : "",
+        alignTop ? "edw-card-aligntop" : "",
+        clickable ? "edw-card-clickable" : "",
+        className,
+      ]
+        .filter(Boolean)
+        .join(" ")}
+      style={style}
+      {...props}
+    >
       {children}
     </div>
   );
