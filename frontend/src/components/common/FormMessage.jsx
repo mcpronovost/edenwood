d924718a954c22e838b0723c652f5618c0540c14
mcpronovost <mcpronovostkurkiency@gmail.com>
2026-01-13 21:08:54 -0500

    update FormMessage common component

--- frontend/src/components/common/FormMessage.jsx
+++ frontend/src/components/common/FormMessage.jsx
@@ -2,11 +2,11 @@ import { useTranslation } from "@/services/translation";
 
 import EdwAlert from "./Alert";
 
-export default function EdwFormMessage({ hasError }) {
+export default function EdwFormMessage({ hasError, style }) {
   const { t } = useTranslation();
 
   return (
-    <div className="edw-form-message">
+    <div className="edw-form-message" style={style}>
       {hasError && <EdwAlert title="Error" message={hasError || t("An error occurred")} variant="danger" />}
     </div>
   );
